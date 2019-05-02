d6d80473cc4349afa2524c0156165d3aebc7ec57
karenlilixoxo@outlook.com <karenlilixoxo@outlook.com>
2019-05-02 09:47:08 -0700

    rota

--- PlantillaOpenGL/Carro.cpp
+++ PlantillaOpenGL/Carro.cpp
@@ -1,6 +1,13 @@
 #include "stdafx.h"
 #include "Carro.h"
 
+void Carro::avanzar() {
+	coordenadas.z += 0.1f;
+}
+void Carro::actualizarMatrizModelo() {
+	modelo = mat4(1.0f);
+	modelo = translate(modelo, coordenadas);
+}
 Carro::Carro() {
 	vertices.push_back({ vec4(-0.5f, 0.40f, -0.7f, 1.0f),vec4((float(rand() % 101)) / 100,(float(rand() % 101)) / 100,(float(rand() % 101)) / 100,1.0f) });
 	vertices.push_back({ vec4(0.5f, 0.40f, -0.7f, 1.0f),vec4((float(rand() % 101)) / 100,(float(rand() % 101)) / 100,(float(rand() % 101)) / 100,1.0f) });
--- PlantillaOpenGL/Carro.h
+++ PlantillaOpenGL/Carro.h
@@ -5,4 +5,10 @@
 
 class Carro : public Modelo {
 public: Carro();
+		void avanzar();
+		void rotar();
+private:
+	void actualizarMatrizModelo();
+	vec3 coordenadas = vec3(0.0f, 0.0f, 0.0f);
+
 };
